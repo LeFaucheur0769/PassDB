1242f498bf09b82526acc2324278f99f6c9decd8
LeFaucheur0769 <kali.lefaucheur@gmail.com>
2025-07-07 01:17:57 +0200

    modified:   src/tools/urlEmailPass.sh

--- src/tools/urlEmailPass.sh
+++ src/tools/urlEmailPass.sh
@@ -12,5 +12,17 @@ OUTPUT="$2"
 # mkdir -p "$OUTPUT_DIR"  
 echo "Sorting $INPUT into $OUTPUT_FILE"
 
-awk -F: '{if (NF >= 3) print $2 ":" $3 ":" $1}' "$INPUT" > "$OUTPUT"
+# awk -F: '{if (NF >= 3) print $2 ":" $3 ":" $1}' "$INPUT" > "$OUTPUT"
+awk '
+{
+    # Match from start: capture URL (greedy up to last 2 colons), password, then email
+    match($0, /^(https?:\/\/[^:]+):([^:]+):([^:]+)$/, groups)
+    if (RSTART > 0) {
+        url = groups[1]
+        email = groups[2]
+        pass = groups[3]
+	      print email ":" pass   
+    }
+}
+' "$OUTPUT" > "$OUTPUT"
 sed -i 's/\r//' "$OUTPUT"
