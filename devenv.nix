eec6afa2a6911c46604a9c9fcf6e6768755ba777
Kali_lefaucheur <kali.lefaucheur@gmail.com>
2026-02-23 00:52:28 +0100

    did some things

--- devenv.nix
+++ devenv.nix
@@ -4,14 +4,31 @@
   config,
   inputs,
   ...
-}: {
+}:
+{
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [pkgs.git pkgs.cargo pkgs.python314 pkgs.python314Packages.pyyaml pkgs.python314Packages.pip pkgs.python314Packages.questionary];
+  packages = [
+    pkgs.git
+    pkgs.cargo
+    pkgs.python314
+    pkgs.python314Packages.pyyaml
+    pkgs.python314Packages.pip
+    pkgs.python314Packages.questionary
+    pkgs.python314Packages.pyside6
+    pkgs.python314Packages.pyqtdarktheme
+    pkgs.qt6.qtbase
+    pkgs.qt6.qtsvg
+    pkgs.qt6.qttools
+    pkgs.qt6.qtdeclarative
+    pkgs.nerd-fonts.fira-code
+    pkgs.python314Packages.black
 
-  # https://devenv.sh/languages/
+  ];
+
+  #https://devenv.sh/languages/
   languages.rust.enable = true;
   languages.python.enable = true;
 
@@ -22,14 +39,17 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
+  scripts = {
+    hello.exec = ''
+      echo hello from $GREET
+    '';
+  };
 
   # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
+    fish
+      hello         # Run scripts directly
+      git --version # Use packages
   '';
 
   # https://devenv.sh/tasks/
@@ -40,8 +60,8 @@
 
   # https://devenv.sh/tests/
   enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
+     echo "Running tests"
+    python GUI/gui.py 
   '';
 
   # https://devenv.sh/git-hooks/
