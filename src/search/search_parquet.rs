4eb56e80e6a07ac40a605794c681ac0b60bba7c9
LeFaucheur0769 <tristanburdin1@gmail.com>
2026-08-10 00:27:23 +0200

    Changed the way i store the db to a sqlite db because it allows more informations to be stored

---
+++ src/search/search_parquet.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use duckdb::Connection;
+
+pub fn search_by_email(
+    email: &str,
+) -> Result<
+    Vec<(
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+        Option<String>,
+    )>,
+> {
+    let conn = Connection::open_in_memory()?;
+    let mut stmt = conn.prepare(
+        "SELECT email, username ,password, url, name, other
+FROM read_parquet('output/sorted/database.parquet')
+WHERE email = ?1",
+    )?;
+    let mut rows = stmt.query([email])?;
+    let mut results = Vec::new();
+
+    while let Some(row) = rows.next()? {
+        results.push((
+            row.get(0)?,
+            row.get(1)?,
+            row.get(2)?,
+            row.get(3)?,
+            row.get(4)?,
+            row.get(5)?,
+        ));
+    }
+
+    Ok(results)
+}
